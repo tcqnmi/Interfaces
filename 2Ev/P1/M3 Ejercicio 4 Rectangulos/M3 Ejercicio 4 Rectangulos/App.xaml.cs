e5ef636db96a244d78bcba0b4b2a345fd8bdc74e
Tamara <t.cqnmi@gmail.com>
2022-01-20 12:11:22 +0100

    Ejercicios de animacion 3 y 4

---
+++ 2Ev/P1/M3 Ejercicio 4 Rectangulos/M3 Ejercicio 4 Rectangulos/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace M3_Ejercicio_4_Rectangulos
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
